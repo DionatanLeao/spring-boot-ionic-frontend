e29c6387a2e1b272fda2f2fe92b83247a6634428
DionatanLeao <dionatan.ti@outlook.com>
2020-01-13 16:19:17 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,16 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { CategoriaDTO } from '../../models/categoria.dto';
-
-
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
